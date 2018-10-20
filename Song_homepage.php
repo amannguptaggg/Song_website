bd9cf32359381c93e0c73e5e808eec4e1e417123
Aman Gupta <44239212+amannguptaggg@users.noreply.github.com>
2018-10-20 21:06:18 +0530

    Add files via upload

---
+++ Song_homepage.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home : Songs</title>
+    <link rel="stylesheet" href="StyleSheet.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-fluid" style="padding:0px;">
+
+        <!-------------------------- Row 0 ------------------>
+        <div class="row" style="background-color:#1a2244">
+
+            <div class="container-fluid">
+                <div class="row ml-2">
+                    <div class=" col-md-9 col-sm-9 col-xs-9" style="background-color:#1a2244; color:#fff; padding:5px;">
+                        <a href="#" style="margin:8px; color:#fff"><i class="fa fa-facebook-square"></i></a>
+                        <a href="#" style="margin:8px; color:#fff"><i class="fa fa-youtube-play"></i></a>
+                        <a href="#" style="margin:8px; color:#fff"><i class="fa fa-twitter"></i></a>
+                        <a href="#" style="margin:8px; color:#fff"><i class="fa fa-google-plus"></i></a>
+                    </div>
+                    <div class=" col-md-3 col-sm-3 col-xs-3">
+                        <a href="#" style="margin:8px; color:#fff"><i class="fa fa-user-circle"></i></a>
+                        <a href="#" style="margin:8px; color:#fff"><i class="fa fa-search"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!---------------------- Row 1 ------------->
+        <div class="row">
+            <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-center" style="background-color:#8342f4; padding:0px;">
+                <img src="header.jpg" class="img img-responsive" style="width:100%;height:100%;">
+            </div>
+        </div>
+
+
+        <!-----------------Row 2 ------------>
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12 text-center" style="background-color:#86f442; padding:0px;">
+                <nav class="navbar navbar-expand-sm navbar-dark bg-dark ">
+                    <ul class="navbar-nav" style="margin-left:30px">
+
+                        <li class="nav-item mr-5"><a class="nav-link" href="#">
+                                Home
+                            </a></li>
+                        <li class="nav-item mr-5"><a class="nav-link" href="#">
+                                Audio Songs
+                            </a></li>
+                        <li class="nav-item mr-5"><a class="nav-link" href="#">
+                                Videos
+                            </a></li>
+                        <li class="nav-item mr-5"><a class="nav-link" href="#">
+                                Contacts
+                            </a></li>
+                    </ul>
+
+
+                </nav>
+            </div>
+        </div>
+
+        <!--------------- Row 3 ------------------->
+        <div class="row">
+            <div class="col-md-3 col-sm-4" style="background-color:#f45942">
+                <div class="row">
+                    <div class="col-sm-12 text-center">
+                        <h4 class="text">Menu</h4>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <table class="table d-flex ml-2 text-center text ">
+                                <tr>
+                                    <td class="OnMouse">
+                                        <h5> Latest Songs</h5>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="OnMouse">
+                                        <h5>Old Songs</h5>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td class="OnMouse">
+                                        <h5> English Songs </h5>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td class="OnMouse">
+                                        <h5>Punjabi Songs</h5>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td class="OnMouse">
+                                        <h5> TOp Selected Album</h5>
+                                    </td>
+                                </tr>
+
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-9 col-sm-8" style="background-color:#4f4f3c; padding:0px;">
+
+                <div class="m-2">
+                    <div class="row m-2">
+                        <div class="col-md-4 col-sm-4 col-lg-4">
+                            <div class="card ">
+                                <div class="card-header">
+                                    <h4 class="card-title"> Bollywood </h4>
+                                    <h5 class="card-subtitle"> Listen Hindi Songs </h5>
+                                </div>
+                                <div class="card-body">
+                                    <img src="img1.jpg" class="img img-responsive card-img">
+                                </div>
+                                <div class="card-footer text-center">
+                                    <a class="card-link" href="#"> Go Now </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-4 col-lg-4">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title"> Bollywood </h4>
+                                    <h5 class="card-subtitle"> Listen Hindi Songs </h5>
+                                </div>
+                                <div class="card-body">
+                                    <img src="img2.jpg" class="img img-responsive card-img">
+                                </div>
+                                <div class="card-footer text-center">
+                                    <a class="card-link" href="#"> Go Now </a>
+                                </div>
+                            </div>
+                        </div>
+                            <div class="col-md-4 col-sm-4 col-lg-4">
+                                <div class="card ">
+                                    <div class="card-header">
+                                        <h4 class="card-title"> Bollywood </h4>
+                                        <h5 class="card-subtitle"> Listen Hindi Songs </h5>
+                                    </div>
+                                    <div class="card-body">
+                                        <img src="img3.jpg" class="img img-responsive card-img">
+                                    </div>
+                                    <div class="card-footer text-center">
+                                        <a class="card-link" href="#"> Go Now </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                        
+                        <div class="row m-2">
+                        <div class="col-md-4 col-sm-4 col-lg-4">
+                            <div class="card ">
+                                <div class="card-header">
+                                    <h4 class="card-title"> Bollywood </h4>
+                                    <h5 class="card-subtitle"> Listen Hindi Songs </h5>
+                                </div>
+                                <div class="card-body">
+                                    <img src="img1.jpg" class="img img-responsive card-img">
+                                </div>
+                                <div class="card-footer text-center">
+                                    <a class="card-link" href="#"> Go Now </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-4 col-lg-4">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title"> Bollywood </h4>
+                                    <h5 class="card-subtitle"> Listen Hindi Songs </h5>
+                                </div>
+                                <div class="card-body">
+                                    <img src="img2.jpg" class="img img-responsive card-img">
+                                </div>
+                                <div class="card-footer text-center">
+                                    <a class="card-link" href="#"> Go Now </a>
+                                </div>
+                            </div>
+                        </div>
+                            <div class="col-md-4 col-sm-4 col-lg-4">
+                                <div class="card ">
+                                    <div class="card-header">
+                                        <h4 class="card-title"> Bollywood </h4>
+                                        <h5 class="card-subtitle"> Listen Hindi Songs </h5>
+                                    </div>
+                                    <div class="card-body">
+                                        <img src="img3.jpg" class="img img-responsive card-img">
+                                    </div>
+                                    <div class="card-footer text-center">
+                                        <a class="card-link" href="#"> Go Now </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                        
+                        <div class="row m-2">
+                        <div class="col-md-4 col-sm-4 col-lg-4">
+                            <div class="card ">
+                                <div class="card-header">
+                                    <h4 class="card-title"> Bollywood </h4>
+                                    <h5 class="card-subtitle"> Listen Hindi Songs </h5>
+                                </div>
+                                <div class="card-body">
+                                    <img src="img1.jpg" class="img img-responsive card-img">
+                                </div>
+                                <div class="card-footer text-center">
+                                    <a class="card-link" href="#"> Go Now </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-4 col-lg-4">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title"> Bollywood </h4>
+                                    <h5 class="card-subtitle"> Listen Hindi Songs </h5>
+                                </div>
+                                <div class="card-body">
+                                    <img src="img2.jpg" class="img img-responsive card-img">
+                                </div>
+                                <div class="card-footer text-center">
+                                    <a class="card-link" href="#"> Go Now </a>
+                                </div>
+                            </div>
+                        </div>
+                            <div class="col-md-4 col-sm-4 col-lg-4">
+                                <div class="card ">
+                                    <div class="card-header">
+                                        <h4 class="card-title"> Bollywood </h4>
+                                        <h5 class="card-subtitle"> Listen Hindi Songs </h5>
+                                    </div>
+                                    <div class="card-body">
+                                        <img src="img3.jpg" class="img img-responsive card-img">
+                                    </div>
+                                    <div class="card-footer text-center">
+                                        <a class="card-link" href="#"> Go Now </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </div>
+
+        <!---------------- Row 4 ------------------------>
+        <div class="row">
+            <div class="col-sm-12 text-center" style="background-color:#327a6b">
+                <br>
+
+                <h5 class="text"> This is Song Website</h5>
+                <br>
+                <h6> Copyright@ 2018 </h6>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ song_LoginPage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link rel="stylesheet" href="StyleSheet.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>NewEra Songs</title>
+</head>
+
+<body class="bg">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 col-sm-12 col-xs-12">
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12">
+                <form id="log">
+                    <h3 class="text heading"> LogIn Now</h3>
+                    <br>
+                    <img class="img img-responsive imgAv" src="avtar.png">
+                    <div class="form-group">
+                        <label class="text">Email</label>
+                        <input type="email" class="form-control" placeholder="Email">
+                        <label class="text">Password</label>
+                        <input type="password" class="form-control" placeholder="Password">
+                        <br>
+                        <div class="checkbox text">
+                            <label>
+                                <input type="checkbox"> Rembermber Password </label>
+                        </div>
+                        <button type="submit" class="btn btn-success btn-block "> Log In </button>
+                    </div>
+
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
